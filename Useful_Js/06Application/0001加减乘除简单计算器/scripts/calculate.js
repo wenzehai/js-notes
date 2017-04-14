2a66963ffa20336ec9bc06b83e3e6390a6e8e39a
Zehai Wen <yamuchashaoye@live.com>
2017-04-15 00:19:25 +0800

    add calculate application

---
+++ Useful_Js/06Application/0001加减乘除简单计算器/scripts/calculate.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/4/14 0014.
+ */
+function add() {
+    var num1 = Number(document.getElementById("number1").value);
+    var num2 = Number(document.getElementById("number2").value);
+    var result = document.getElementById("result");
+    result.value = num1 + num2;
+}
+
+
+
