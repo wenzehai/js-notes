a603425491f6905bda2a5848be857112d7db1912
zehai <373952776@qq.com>
2018-01-10 18:03:36 +0800

    优化共享宝订单页面,新增vue框架

--- gongxiangbao/pages/bills/bills.js
+++ gongxiangbao/pages/bills/bills.js
@@ -1,10 +1,20 @@
 Page({
   data: {
-
+    isShow: true,
+    currentTab: 0,
   },
-  toTapbar:function(){
-    wx.switchTab({
-      url: '/pages/bills/bills'
-    })
+  tabNav: function (e) {
+    console.log(e.target.dataset.current, 111, this.data.currentTab)
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+
+      var showMode = e.target.dataset.current == 0;
+
+      this.setData({
+        currentTab: e.target.dataset.current,
+        isShow: showMode
+      })
+    }
   }
 })
