ce89d4487740cc3014a650034841700c7a9fd042
zehai <373952776@qq.com>
2018-01-12 20:13:18 +0800

    新增相应式vue框架，还在搬代码

---
+++ vue-cli/framwork/framework/src/components/SearchForm.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="searchForm">
+      <div class="container">
+        <el-row class="panel">
+          <el-col class="panel_hd">
+            <h1 class="panelTitle">搜索选项<i class="el-icon-warning"></i></h1>
+          </el-col>  
+          <el-col class="panel_bd">
+            <el-form ref="form" :model="sizeForm" label-width="80px" size="mini">
+            <el-form-item label="活动名称">
+              <el-input v-model="sizeForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="活动区域">
+              <el-select v-model="sizeForm.region" placeholder="请选择活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="活动时间">
+              <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期" v-model="sizeForm.date1" style="width: 100%;"></el-date-picker>
+              </el-col>
+              <el-col class="line" :span="2">-</el-col>
+              <el-col :span="11">
+                <el-time-picker type="fixed-time" placeholder="选择时间" v-model="sizeForm.date2" style="width: 100%;"></el-time-picker>
+              </el-col>
+            </el-form-item>
+            <el-form-item label="活动性质">
+              <el-checkbox-group v-model="sizeForm.type">
+                <el-checkbox-button label="美食/餐厅线上活动" name="type"></el-checkbox-button>
+                <el-checkbox-button label="地推活动" name="type"></el-checkbox-button>
+                <el-checkbox-button label="线下主题活动" name="type"></el-checkbox-button>
+              </el-checkbox-group>
+            </el-form-item>
+            <el-form-item label="特殊资源">
+              <el-radio-group v-model="sizeForm.resource" size="medium">
+                <el-radio border label="线上品牌商赞助"></el-radio>
+                <el-radio border label="线下场地免费"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+            <el-form-item size="large">
+              <el-button type="primary" @click="onSubmit">立即创建</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+          </el-col>  
+        </el-row>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "SearchForm",
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      
+      tableData: Array(20).fill(item),
+      sizeForm: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: ""
+      },
+      activeName2: "first"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    onSubmit() {
+      console.log("submit!");
+    },
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
+
+<style scoped >
+.main {
+  background: #f6f6f6;
+}
+
+.panel {
+  margin-bottom: 25px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 10px;
+}
+.panelTitle {
+  font-size: 16px;
+  font-weight: bold;
+}
+.panel_hd {
+  margin-bottom: 25px;
+}
+
+</style>
---
+++ vue-cli/framwork/src/components/MainNav.vue
@@ -0,0 +1,87 @@
+<template>
+    <div id="_mainNav">
+        <el-header class="mainNav" height="75px">
+          <div class="container">
+            <el-row>
+              <!-- logo -->
+              <el-col :span="2">
+                <router-link to="/home" class="link"><img src="../assets/images/templogo.png" alt="" class="logo"></router-link>
+              </el-col>
+              <!-- 菜单 -->
+              <el-col :span="20">
+                <el-menu :default-active="activeIndex" class="mainMenu" mode="horizontal" @select="handleSelect">
+                  <el-menu-item index="1"><router-link to="/home" class="link">首页</router-link></el-menu-item>
+                  <el-menu-item index="2"><router-link to="/search" class="link">查询</router-link></el-menu-item>
+                  <el-submenu index="3">
+                    <template slot="title">我的工作台</template>
+                    <el-menu-item index="3-1"><router-link to="/home" class="horizLink">选项1</router-link></el-menu-item>
+                    <el-menu-item index="3-2"><router-link to="/home" class="horizLink">选项2</router-link></el-menu-item>
+                    <el-menu-item index="3-3"><router-link to="/home" class="horizLink">选项3</router-link></el-menu-item>
+                  </el-submenu>
+                   <el-menu-item index="4"><router-link to="/create" class="link">新增</router-link></el-menu-item>
+                </el-menu>
+              </el-col>
+              <el-col :span="2">
+                <router-link to="/create" class="link"><el-button type="primary" icon="el-icon-circle-plus-outline" size="small" class="createBtn">新建</el-button></router-link>
+              </el-col>
+            </el-row>
+          </div>
+        </el-header>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'mainNav',
+    data: function() {	
+        return {
+            activeIndex: "1",
+            activeIndex2: "1"
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+        },
+        handleClick(tab, event) {
+        console.log(tab, event);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.mainNav {
+  position: fixed;
+  z-index: 9;
+  left: 0;
+  top: 30px;
+  width: 100%;
+  border-bottom: 1px solid #f6f6f6;
+  background: #fff;
+}
+.logo {
+  width: 70px;
+  height: 70px;
+}
+.mainMenu {
+  margin-top: 5px;
+  border-bottom: none;
+}
+.link {
+    text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+}
+.horizLink {
+  text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+}
+.createBtn {
+  margin-top: 18px;
+}
+
+</style>
